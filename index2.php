2ed847a44c6111bf620a28d43813a2a50993d857
pmartinp <pmartinpdev@gmail.com>
2022-11-24 17:59:21 +0100

    Archivos del examen iniciales

---
+++ index2.php
@@ -0,0 +1,8 @@
+<?php
+/*include "Disco.php";
+
+$miDisco = new Disco("Origen", 24, 15, "es,en,fr", "16:9");
+echo "<strong>" . $miDisco->titulo . "</strong>";
+echo "<br>Precio: " . $miDisco->getPrecio() . " euros"; 
+echo "<br>Precio IVA incluido: " . $miDisco->getPrecioConIva() . " euros";
+$miDisco->muestraResumen();
---
+++ index3.php
@@ -0,0 +1,8 @@
+<?php
+/*include "Juego.php";
+
+$miJuego = new Juego("God of War: Ragnarök", 26, 49.99, "PS4", 1, 1); 
+echo "<strong>" . $miJuego->titulo . "</strong>"; 
+echo "<br>Precio: " . $miJuego->getPrecio() . " euros"; 
+echo "<br>Precio IVA incluido: " . $miJuego->getPrecioConIva() . " euros";
+$miJuego->muestraResumen();
