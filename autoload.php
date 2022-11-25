97621ffa6eb6619da82bc1f4a11b1b3a30458f2a
pmartinp <pmartinpdev@gmail.com>
2022-11-25 16:50:38 +0100

    actualizacion autoload.php para que funcione en linux

--- autoload.php
+++ autoload.php
@@ -2,7 +2,8 @@
 declare(strict_types=1);
 
 function autoload($className){
-    include_once("..\\".$className.".php");
+    $path = str_replace("\\", "/", $className);
+    include_once("../".$path.".php");
 }
 
 spl_autoload_register('autoload');
